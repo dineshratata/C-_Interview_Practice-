7204e154f624721df144d13153cb1755c8352d10
dineshratata <dineshstallone15196@gmail.com>
2023-12-07 15:36:52 +0530

    new change

---
+++ PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+   
+    public class Prime
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Number :");
+            int number = int.Parse(Console.ReadLine());
+            bool isprime = true;
+
+
+            for (int i = 2; i < number / 2; i++)
+            {
+
+                if (number % 2 == 0)
+                {
+
+                    isprime = false;
+
+                    break;
+
+                }
+            }
+
+                if (isprime)
+                {
+
+                    Console.WriteLine($"Number {number}is prime");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Number {number} is not prime");
+
+                }
+                
+
+            }
+
+
+        }
+
+
+    }
+
+
+
